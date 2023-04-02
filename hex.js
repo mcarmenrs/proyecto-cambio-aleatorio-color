cc07db8aa58ed1dda6c21261cfc5b3522f8579e7
mcarmenrs <mcarmen.rs79@gmail.com>
2023-04-02 07:38:24 +0000

    estilos

--- hex.js
+++ hex.js
@@ -6,7 +6,7 @@ const color = document.querySelector(".color");
 btn.addEventListener('click', function(){
     let hexColor = '#';
     for(let i = 0;i<6;i++) {
-        hexColor += hex[11]
+        hexColor += hex[getRandomNumber()]
     }
 
     color.textContent = hexColor;
